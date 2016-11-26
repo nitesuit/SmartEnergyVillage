f66306863d231a079edfbff035b812c98fead7bc
paulius <paulius_puodziunas@fmail.co.uk>
2016-11-26 14:22:51 +0200

    Added turbine and other stuff. Blah.. blah.

---
+++ Assets/FancySensorFramework/SolarPanel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public  class SolarPanel : MonoBehaviour
+{
+	public  WebCamTexture webCamText;
+
+	public  float GetCameraLevel()
+	{
+		var colorArr = webCamText.GetPixels();
+		var avgColor = new Color();
+		
+		foreach (var c in colorArr)
+		{
+			avgColor += c;
+		}
+		avgColor = avgColor / colorArr.Length;
+		
+		//if (testImage != null)
+		//{
+		//	testImage.material.color = avgColor;
+		//}
+
+		return (avgColor.r + avgColor.g + avgColor.b)/3f;
+		
+	}
+
+	 void StartCameraSample()
+	{
+		webCamText = new WebCamTexture();
+		webCamText.Play();
+	}
+
+	 void StopCameraSample()
+	{
+		webCamText.Stop();
+	}
+
+}
