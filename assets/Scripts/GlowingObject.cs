8c5c109be56dad3239a35c627216f9ef03ad4ba1
Gediminas Petrikas <gedp@mobilelife.dk>
2016-11-26 13:29:26 +0200

    Add glow

---
+++ assets/Scripts/GlowingObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GlowingObject : MonoBehaviour {
+	public bool ShouldGlow { get; set; }
+
+	void Update() {
+		if (!ShouldGlow) {
+			return;
+		}
+
+		Renderer renderer = GetComponent<Renderer> ();
+		Material mat = renderer.material;
+
+		float emission = Mathf.PingPong (Time.time, 1.0f);
+		Color baseColor = Color.white; //Replace this with whatever you want for your base color at emission level '1'
+
+		Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+
+		mat.SetColor ("_EmissionColor", finalColor);
+	}
+
+}
