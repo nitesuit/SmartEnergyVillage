ce9fc8a7d45816924825deb8cee5b0ede6595337
Povilas Staskus <povilas.staskus@gmail.com>
2016-11-26 16:12:02 +0200

    GridManager

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager : MonoBehaviour {
+
+	public enum GridState { On, Off, Blink }
+	public GridState State {
+		set {
+			State = value;
+			if (State == GridState.On) {
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Blink() {
+		
+	}
+
+	void TurnOn() {
+		
+	}
+
+	void TurnOff() {
+		
+	}
+}
