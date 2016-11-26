bcdbee6627f57cf96de2ae3b7b71d14985565103
Gediminas Petrikas <gedp@mobilelife.dk>
2016-11-26 12:14:20 +0200

    Create gameManager

---
+++ assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour {
+	
+	public int Score { get; set; }
+	public List<GameObject> Objects { get; set; }
+
+
+	public GameObject GetNextObject () {
+		return new GameObject (); 
+	}
+}
