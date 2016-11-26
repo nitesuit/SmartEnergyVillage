0c17b92e4c769ad97b41a3bab36c2bff18ff42c6
Povilas Staskus <povilas.staskus@gmail.com>
2016-11-26 13:16:26 +0200

    GameManager

---
+++ Assets/Scripts/ExampleManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleManager : MonoBehaviour {
+
+	public GameManager GameManager;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (GameManager.NextObject);
+		GameManager.CompleteLevel ();
+		Debug.Log (GameManager.NextObject);
+		GameManager.CompleteLevel ();
+		Debug.Log (GameManager.NextObject);
+		GameManager.CompleteLevel ();
+	}
+	
+
+}
--- assets/Scripts/GameManager.cs
+++ assets/Scripts/GameManager.cs
@@ -3,12 +3,60 @@ using System.Collections.Generic;
 
 
 public class GameManager : MonoBehaviour {
-	
+
+	enum GameScore { Win = 100, Lose = -100, Start = 0};
+
 	public int Score { get; set; }
-	public List<GameObject> Objects { get; set; }
+	public List<GameObject> Objects;
+	private GameObject _nextGameObject;
+	private int _currentLevel = 0;
 
-
-	public GameObject GetNextObject () {
-		return new GameObject (); 
+	void Update() {
+		checkGameStatus ();
 	}
+
+	public GameObject NextObject
+	{
+		get
+		{
+			if (Objects.Count > _currentLevel) {
+				return Objects[_currentLevel];
+			} else {
+				Debug.Log ("No more game objects. Resetting level to 0");
+				_currentLevel = 0;
+				return Objects[_currentLevel];
+			}
+		}
+	}
+
+	public void CompleteLevel() {
+		_currentLevel++;
+	}
+
+
+	public void startGame() {
+		Score = 0;
+		// do something when game started
+	}
+
+	//Private functions
+
+	private void checkGameStatus() {
+		if (Score == (int)GameScore.Lose) {
+			gameLost ();
+		}
+
+		if (Score == (int)GameScore.Win) {
+			gameWon ();
+		}
+	}
+
+	private void gameLost() {
+		Debug.Log ("Game Lost");
+	}
+
+	private void gameWon() {
+		Debug.Log ("Game Won");
+	}
+	
 }
