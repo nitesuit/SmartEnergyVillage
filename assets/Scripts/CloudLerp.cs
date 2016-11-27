50d64cd2ac8cb89d1c3f37c444ce29d609e2c284
Gediminas Petrikas <gedp@mobilelife.dk>
2016-11-27 13:27:39 +0200

    Add cloud move

---
+++ assets/Scripts/CloudLerp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudLerp : MonoBehaviour {
+	public bool ShouldMove { get; set; }
+
+	void Update() {
+		if (ShouldMove) {
+			return;
+		}
+		float finalSpeed = Time.deltaTime * 5f;
+		transform.position = new Vector3 (transform.position.x - finalSpeed, transform.position.y, transform.position.z);
+	}
+}
