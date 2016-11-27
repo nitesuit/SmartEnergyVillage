70993dfee9d528a750316ffe06d356b41e37ff39
paulius <paulius_puodziunas@fmail.co.uk>
2016-11-27 12:50:50 +0200

    Fixed turbine.

---
+++ Assets/CameraLerp.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraLerp : MonoBehaviour {
+
+	public bool moveToStation;
+	public bool isMoving;
+
+	public float checkPointTime = 2f;
+
+	public List<Transform> moveMoveToStationCheckPoints = new List<Transform>();
+
+	void Update()
+	{
+		if (moveToStation && !isMoving)
+		{
+			StartCoroutine(MoveByPath(moveMoveToStationCheckPoints));
+			isMoving = true;
+		}
+
+	}
+	
+	IEnumerator MoveByPath(List<Transform> path)
+	{
+
+		foreach (var point in path)
+		{
+			var timer = 0f;
+			while (timer < checkPointTime)
+			{
+				var target = new Vector3(point.position.x, transform.position.y, point.position.z);
+				transform.position = Vector3.Slerp(transform.position, target, checkPointTime);
+				
+				timer += Time.deltaTime;
+				yield return null;
+			}
+		}
+		
+	}
+
+	
+}
